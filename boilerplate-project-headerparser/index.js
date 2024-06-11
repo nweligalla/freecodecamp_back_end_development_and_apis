6f2bd1cf884d579438137b31b9862fdf5285d9a1
nweligalla <nayanaweligalla@gmail.com>
2024-06-11 18:30:59 +0530

    added Request Header Parser Microservice submission

--- boilerplate-project-headerparser/index.js
+++ boilerplate-project-headerparser/index.js
@@ -19,9 +19,14 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+
+  // console.log(req)
+  res.json({ 
+    ipaddress: req.ip,
+    language: req.get('Accept-Language'),
+    software : req.get('User-Agent')
+  });
 });
 
 // listen for requests :)
